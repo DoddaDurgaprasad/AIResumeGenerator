3e52b26da329a8f910585a896a2cc46a091e1ae8
Durga Prasad Dodda <nanidodda7436@gmail.com>
2026-07-08 18:29:17 +0530

    resume generation

--- server/src/services/aiService.js
+++ server/src/services/aiService.js
@@ -141,6 +141,7 @@ async function generatePdfFromHtml(htmlContent) {
 
     const browser = await puppeteer.launch({
         headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable', 
         args: [
             "--no-sandbox",
             "--disable-setuid-sandbox",
